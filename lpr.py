c4e9233d39d71b6ed7a10d7c0ac7fa6c418b65b1
Ankit Dixit <akd7071@gmail.com>
2021-01-31 03:24:55 +0530

    converted image to grayscale

--- lpr.py
+++ lpr.py
@@ -1,13 +1,18 @@
 from cv2 import cv2
-import imutils # to resize our image
+import imutils 
 import pytesseract
 
 pytesseract.pytesseract.tesseract_cmd =r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
-#now to read the image file
-image = cv2.imread("1.png")
-#we will resize and standardize our image to 500
+# Reading the image file
+
+image = cv2.imread("1.jpg") 
 image=imutils.resize(image, width=500)
-#we will display orignal image when it will start finding
-cv2.imshow("Orignal Image", image) #here orignal image is the name of window /// you can give your suitable name
-cv2.waitKey(0) #till i press anything it will not execute further
+cv2.imshow("Orignal Image", image) 
+
+
+# Image Conversion to grayscale
+
+gray = cv2.cvtColor(image , cv2.COLOR_BGR2GRAY)
+cv2.imshow("Gray Scale Image", gray)
+cv2.waitKey(0)
